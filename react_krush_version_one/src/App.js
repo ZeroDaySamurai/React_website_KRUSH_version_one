37a3eeab265c5da9604dc694afc32ff278a337a7
Shakeel Hoosain <62466466+ZeroDaySamurai@users.noreply.github.com>
2021-10-10 13:46:31 +0200

    Third Commit

--- react_krush_version_one/src/App.js
+++ react_krush_version_one/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from './pages/Services';
-import About from './pages/Products';
+import About from './pages/About';
 import Contact from './pages/SignUp';
 
 
@@ -15,7 +15,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' exact component={Services} />
-          <Route path='/products' exact component={About} />
+          <Route path='/about' exact component={About} />
           <Route path='/sign-up' exact component={Contact} />
         </Switch>
       </Router>
--- react_krush_version_one/src/pages/About.js
+++ react_krush_version_one/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../src/App'
+import '../App'
 
 export default function Products() {
     return( 
